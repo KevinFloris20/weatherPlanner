7a5082aa9f4b274ab0e3f64069a6962814dec8c0
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-02-12 04:59:50 -0500

    fixed localstore and other errors

--- script.js
+++ script.js
@@ -9,25 +9,22 @@ var humid = "";
 var uv = "";
 var icon;
 var condition = "";
-var history;
 var FORcast;
-var localstore = "";
 var img = " ";
-//+future 5 day forcast
+var vardata;
+var localstore2;
+
+
 //math temprature
 function temp1(temp){
     return ((temp - 273.15) * 9/5 + 32).toFixed(0);
- }
-
-function store(inpu){
-input.value = inpu;
 }
 
 
 //call api + get data
 function getweather(){
+    forcas.innerHTML = "";//clear forcast cashe
     var input = document.getElementById("input").value;
-    localstore += input + ","
     console.log("yoo");
         $.ajax({
         type: "GET", 
@@ -43,8 +40,13 @@ function getweather(){
             console.log(humid);
             console.log(data);        
             condition = data.weather[0].description;
-            history = data.weather[0].main;
-            
+            // history = data.weather[0].main;
+            if(input != undefined){
+                if(!(previous.includes(input))){
+                previous.push(input);
+                window.localStorage.setItem("previous", JSON.stringify(previous));
+                }
+            }
         }
     });
     $.ajax({
@@ -52,49 +54,63 @@ function getweather(){
         url: "http://api.openweathermap.org/data/2.5/forecast?q="+input+"&appid=0013900bdb5821e8627c8eccc8185316",
         dataType: "json",
         success: function(data){
+            //set data
             console.log(data);
-                //run the code needed to display
-                function rundata(){
-                    document.getElementById("h2out").innerHTML = '5 day forcast:'
-                    for(var i=0; i < 40; i =i+8){    
-                        FORcast = data.list[i].dt_txt;
-                        console.log(data.list[i].weather[0].main);
-                        if(data.list[i].weather[0].main == "Rain"){
-                            img = '<img style = "width:100px" src="rainy.png">' 
-                        }
-                        if(data.list[i].weather[0].main  == "Sunny"){
-                            img = '<img style = "width:100px" src="sunny.jpg">' 
-                        }
-                        if(data.list[i].weather[0].main  == "Clear"){
-                            img = '<img style = "width:100px" src="clear.jpg">' 
-                        }
-                        if(data.list[i].weather[0].main  == "Snow"){
-                            img = '<img style = "width:100px" src="snowy.jpg">' 
-                        }
-                        if(data.list[i].weather[0].main  == "Clouds"){
-                            img = '<img style = "width:100px" src="cloudy.jpg">' 
-                        }
-                        console.log(FORcast);
-                        forcas.innerHTML += '<div class="col"><p>' + FORcast +'<br>' +'<br>'+ data.list[i].weather[0].main + '<br>'+ img +'<br>'+ data.list[i].weather[0].description + '<br>' + temp1(data.list[i].main.temp) + '&#176;F</p></div>';
-                    }
-                }
+            vardata = data
 
-                document.getElementById("todayout").innerHTML = '<div class="collarge" style="padding:10px">Today<br>'+ condition  + '<br>Humidity:' + humid + '<br>Temprature:' + temp1(temp) + '&#176;F<br>Wind Speed:' + wind  + '<br>' + '<input type="button" id = "nextbtn" value="Go to 5 day forcast"></div>'
-                document.getElementById("nextbtn").addEventListener("click", rundata());
+            //add event listener + today conditions
+            document.getElementById("todayout").innerHTML = '<div class="collarge" style="padding:10px">Today in '+ input +'<br>'+ condition  + '<br>Humidity:' + humid + '<br>Temprature:' + temp1(temp) + '&#176;F<br>Wind Speed:' + wind  + '<br>' + '<input type="button" id = "nextbtn" value="Go to 5 day forcast"></div>'
+            document.getElementById("nextbtn").addEventListener("click", rundata);
         }
     });
-    
+
     //local storage
-    var res = localstore.split(",");
-    localstore = '';
-    for(var i = 0; i <res.length; i++){
-        localstore = localstore + '<input type="button" value="'+res[i]+'" onclick = "store('+res[i]+')"';
+    for(let i=0; i < previous.length; i++){
+        if(document.getElementById(previous[i]) == null){
+            var localstore = document.createElement('div');
+            localstore.innerHTML = '<input type="button" id="' + previous[i] + '" value="' + previous[i] + '" />'; //onclick = "' + textboxedit(previous[i]) + '"
+            document.getElementById("savee").append(localstore);
+        }
     }
-    console.log(localstore);
-    localStorage.setItem("input", localstore);
-    document.getElementById("savee").innerHTML = localStorage.getItem("input");
-    console.log("yo");
 }
+
+//code needed to display forcast
+function rundata(){
+    forcas.innerHTML = "";
+    document.getElementById("h2out").innerHTML = '5 Day Forcast:'
+    for(var i=0; i < 40; i =i+8){    
+        FORcast = vardata.list[i].dt_txt;
+        console.log(vardata.list[i].weather[0].main);
+        if(vardata.list[i].weather[0].main == "Rain"){
+            img = '<img style = "width:100px" src="images/rainy.png">' 
+        }
+        if(vardata.list[i].weather[0].main  == "Sunny"){
+            img = '<img style = "width:100px" src="images/sunny.jpg">' 
+        }
+        if(vardata.list[i].weather[0].main  == "Clear"){
+            img = '<img style = "width:100px" src="images/clear.jpg">' 
+        }
+        if(vardata.list[i].weather[0].main  == "Snow"){
+            img = '<img style = "width:100px" src="images/snowy.jpg">' 
+        }
+        if(vardata.list[i].weather[0].main  == "Clouds"){
+            img = '<img style = "width:100px" src="images/cloudy.jpg">' 
+        }
+        console.log(FORcast);
+        forcas.innerHTML += '<div class="col"><p>' + FORcast +'<br>' +'<br>'+ vardata.list[i].weather[0].main + '<br>'+ img +'<br>'+ vardata.list[i].weather[0].description + '<br>' + temp1(vardata.list[i].main.temp) + '&#176;F</p></div>';
+    }
+}
+
+//get local storage
+var previous = JSON.parse(window.localStorage.getItem("previous")) || [];
+
+//change the value of the textbox 
+$(".savee").click(function(event){
+    const element = event.target;
+    document.getElementById("input").value = element.value;
+    console.log(element.value);
+    getweather;
+});
 
 //0013900bdb5821e8627c8eccc8185316 location api
 //http://api.openweathermap.org/data/2.5/forecast?id=
